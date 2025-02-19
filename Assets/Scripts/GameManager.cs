4de998f69b75efea072b96b46844b539f087447a
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2025-02-19 14:23:08 -0500

    feat(wip): tiles can be selected and deselected

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     private List<LetterTile>[] letterTiles;
+    private List<(int col, int row)> selectedTiles;
+
     private const float letterBaseYOdd = -4.5f;
     private const float letterBaseYEven = -4f;
     private const float letterDeltaY = 0.8f;
@@ -38,6 +40,7 @@ public class GameManager : MonoBehaviour
 
         // initialize lettertiles
         letterTiles = new List<LetterTile>[7];
+        selectedTiles = new List<(int col, int row)>();
         for (int i = 0; i < letterTiles.Length; i++)
         {
             List<LetterTile> current = new List<LetterTile>();
@@ -50,15 +53,13 @@ public class GameManager : MonoBehaviour
                 float x = letterBaseX + (letterDeltaX * i);
                 float y = isEven ? letterBaseYEven + (letterDeltaY * j) : letterBaseYOdd + (letterDeltaY * j);
                 LetterTile newTile = Instantiate(letterTilePrefab, new Vector3(x, y, 0), Quaternion.identity, transform);
-                newTile.Initialize(NextLetter());
+                newTile.Initialize(NextLetter(), i, j, LetterTile.TileType.Normal);
                 current.Add(newTile);
             }
 
             letterTiles[i] = current;
         }
     }
-
-    // TODO: pre calculate word score for each word
 
     // Randomly pick a letter according to letter probability distribution
     private char NextLetter()
@@ -76,5 +77,57 @@ public class GameManager : MonoBehaviour
         }
 
         return csvReader.letterList.letters[^1].letter; // should not happen
+    }
+
+    /// <summary>
+    /// Each <c>LetterTile</c> will call this when they are clicked.
+    /// The appropriate action will be taken (e.g. activate tile, accept word...)
+    /// </summary>
+    /// <param name="column">Column (y) outer index</param>
+    /// <param name="row">Row (x) inner index</param>
+    public void OnTileClick(int column, int row)
+    {
+        // if tile clicked is in the selected tiles list, deselect all after it
+        int index = selectedTiles.IndexOf((column, row));
+        if (index != -1)
+        {
+            for (int i = index; i < selectedTiles.Count; i++)
+            {
+                (int col, int row) coordToDeselect = selectedTiles[i];
+                letterTiles[coordToDeselect.col][coordToDeselect.row].SetIsSelected(false);
+            }
+            selectedTiles.RemoveRange(index, selectedTiles.Count - index);
+
+            // then, readd the selected tile
+            letterTiles[column][row].SetIsSelected(true);
+            selectedTiles.Add((column, row));
+            return;
+        }
+
+        // if the tile clicked is not selected
+        // start new word if no selected tiles yet
+        if (selectedTiles.Count == 0)
+        {
+            selectedTiles.Add((column, row));
+            letterTiles[column][row].SetIsSelected(true);
+        } else
+        {
+            // else, add to selected tiles if adjacent
+            (int col, int row) mostRecentTile = selectedTiles[^1];
+            if (Mathf.Abs(mostRecentTile.col - column) <= 1 && Mathf.Abs(mostRecentTile.row - row) <= 1)
+            {
+                letterTiles[column][row].SetIsSelected(true);
+                selectedTiles.Add((column, row));
+            }
+            else
+            {
+                // else, clear selected
+                foreach ((int col, int row) tile in selectedTiles)
+                {
+                    letterTiles[tile.col][tile.row].SetIsSelected(false);
+                }
+                selectedTiles.Clear();
+            }
+        }
     }
 }
--- Assets/Scripts/LetterTile.cs
+++ Assets/Scripts/LetterTile.cs
@@ -5,17 +5,58 @@ using UnityEngine.EventSystems;
 public class LetterTile : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private TMP_Text letterText;
+    [SerializeField] private GameObject normalSprite;
+    [SerializeField] private GameObject normalSelectedSprite;
 
     private char letter;
+    private int column; // y; outer index
+    private int row; // x; inner index
+    private TileType tileType;
+    private bool isSelected;
 
-    public void Initialize(char letter)
+    public enum TileType { Normal, Fire, Bonus, Gold, Diamond }
+
+    public void Initialize(char letter, int column, int row, TileType type)
     {
         this.letter = letter; // TODO: special case for Q
         letterText.text = letter.ToString();
+        this.column = column;
+        this.row = row;
+        tileType = type;
+        isSelected = false;
+        ApplySprite();
+    }
+
+    // apply the correct sprite based on current state
+    private void ApplySprite()
+    {
+        // disable everything
+        // TODO: implement all other tile types
+        normalSprite.SetActive(false);
+        normalSelectedSprite.SetActive(false);
+
+        switch (tileType)
+        {
+            case TileType.Normal:
+                if (isSelected)
+                {
+                    normalSelectedSprite.SetActive(true);
+                } else
+                {
+                    normalSprite.SetActive(true);
+                }
+                break;
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("clicked me");
+        GameManager.instance.OnTileClick(column, row);
+    }
+
+    public void SetIsSelected(bool isSelected)
+    {
+        this.isSelected = isSelected;
+        ApplySprite();
     }
 }
