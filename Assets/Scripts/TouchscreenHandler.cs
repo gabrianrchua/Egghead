ef63d9eb77c0320da0ba3cc6957dc64057ec5ec7
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2026-05-12 21:34:52 -0400

    fix: missing reference error after switching scenes

--- Assets/Scripts/TouchscreenHandler.cs
+++ Assets/Scripts/TouchscreenHandler.cs
@@ -9,41 +9,66 @@ public class TouchscreenHandler : MonoBehaviour
     private Vector2 currentScreenPosition;
     private LetterTile currentTile;
 
-    private void Awake()
+    private void OnEnable()
     {
+        screenPosition.performed += OnScreenPositionPerformed;
+        press.canceled += OnPressCanceled;
+
         screenPosition.Enable();
         press.Enable();
-        screenPosition.performed += context =>
+    }
+
+    private void OnDisable()
+    {
+        screenPosition.performed -= OnScreenPositionPerformed;
+        press.canceled -= OnPressCanceled;
+
+        screenPosition.Disable();
+        press.Disable();
+
+        currentTile = null;
+    }
+
+    private void OnScreenPositionPerformed(InputAction.CallbackContext context)
+    {
+        if (mainCamera == null)
         {
-            currentScreenPosition = context.ReadValue<Vector2>();
-            //Debug.Log(currentScreenPosition);
-            //mainCamera.ScreenToWorldPoint(currentScreenPosition);
-            Ray ray = mainCamera.ScreenPointToRay(currentScreenPosition);
-            /*if (Physics.Raycast(ray, out RaycastHit hit))
+            mainCamera = Camera.main;
+            if (mainCamera == null)
             {
-                Debug.Log("hit " + hit.collider.name);
-            }*/
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-            if (hit.collider != null)
+                return;
+            }
+        }
+
+        currentScreenPosition = context.ReadValue<Vector2>();
+        //Debug.Log(currentScreenPosition);
+        //mainCamera.ScreenToWorldPoint(currentScreenPosition);
+        Ray ray = mainCamera.ScreenPointToRay(currentScreenPosition);
+        /*if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log("hit " + hit.collider.name);
+        }*/
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        if (hit.collider != null)
+        {
+            if (hit.collider.TryGetComponent<LetterTile>(out LetterTile tile))
             {
-                if (hit.collider.TryGetComponent<LetterTile>(out LetterTile tile))
+                if (currentTile != tile)
                 {
-                    if (currentTile != tile)
-                    {
-                        currentTile = tile;
-                        tile.OnPointerClick();
-                    }
+                    currentTile = tile;
+                    tile.OnPointerClick();
                 }
             }
-        };
-        /*press.performed += _ =>
+        }
+    }
+
+    /*press.performed += _ =>
         {
             Debug.Log("press performed");
         };*/
-        press.canceled += _ =>
-        {
-            currentTile = null;
-        };
 
+    private void OnPressCanceled(InputAction.CallbackContext context)
+    {
+        currentTile = null;
     }
 }
