97f053f6ebaf2b51fa6947278fc5811730d64120
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2026-06-19 19:35:12 -0400

    feat: add animations to auth

--- Assets/Scripts/SelfDeleter.cs
+++ Assets/Scripts/SelfDeleter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SelfDeleter : MonoBehaviour
@@ -10,5 +11,21 @@ public class SelfDeleter : MonoBehaviour
     public void DisableSelf()
     {
         gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Disables self after one frame. Used when calling as an animation event
+    /// to disable self safely without throwing errors, especially with
+    /// <c>TMP_InputField</c> on children components.
+    /// </summary>
+    public void DisableSelfSafely()
+    {
+        StartCoroutine(DisableNextFrame());
+    }
+
+    private IEnumerator DisableNextFrame()
+    {
+        yield return null;
+        DisableSelf();
     }
 }
